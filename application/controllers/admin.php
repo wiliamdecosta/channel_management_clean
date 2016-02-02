fe0e04f966513d6098fd325df17841ece83725f0
wiliamdecosta <wiliamdecosta@gmail.com>
2016-02-02 09:59:44 +0700

    ''

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -15,6 +15,7 @@ class Admin extends CI_Controller
         $this->load->model('M_user');
         $this->load->model('M_admin');
         $this->load->model('M_jqGrid', 'jqGrid');
+        $this->load->model('Gen_id');
 
     }
 
@@ -267,6 +268,9 @@ class Admin extends CI_Controller
 
             if ($countMenu > 0) {
                 $tmp = array_merge($tmp, array('checked' => true));
+                $tmp = array_merge($tmp, array('app_menu_profile_id' => $tmpCount[0]['APP_MENU_PROFILE_ID']));
+            }else {
+                $tmp = array_merge($tmp, array('app_menu_profile_id' => ''));
             }
 
             $data[$i] = $tmp;
@@ -380,5 +384,76 @@ class Admin extends CI_Controller
         } else {
             echo "Update Gagal";
         }
+    }
+    
+    public function getPrivilegeMenuTable() {
+        
+        $app_menu_profile_id = $this->input->post('app_menu_profile_id');
+        $arr_privilege_menu  = $this->M_admin->getPrivilegeMenu($app_menu_profile_id);
+        
+        $strOutput = "";
+        
+        echo '<form method="post" id="form-privelege" action="'.site_url('admin/setPrivilegeMenu').'">';
+        echo '<table width="100%" class="table table-striped table-bordered">';
+        echo '<tr>
+                <th>No</th>
+                <th>Keterangan</th>
+                <th>Status</th>
+                <th>Tgl Pembuatan</th>
+                <th>Dibuat Oleh</th>
+                <th>Tgl Update</th>
+                <th>Diupdate Oleh</th>
+              </tr>';
+        
+        $no=1;
+        foreach($arr_privilege_menu as $item) {
+            
+            $options = array('Y' => 'ACTIVE',
+                            'N' => 'NOT ACTIVE');
+            
+            echo '<tr>';
+                echo '<td>'.$no++.'</td>';
+                echo '<td><label>'.$item->CODE.'</label></td>';
+                echo '<td>'.form_dropdown('opt_status[]', $options, $item->IS_ACTIVE).'</td>';
+                echo '<td><label>'.$item->CREATION_DATE.'</label></td>';
+                echo '<td><label>'.$item->CREATED_BY.'</label></td>';
+                echo '<td><label>'.$item->UPDATE_DATE.'</label></td>';
+                echo '<td><label>'.$item->UPDATED_BY.'</label>'.form_hidden('object_type_id[]', $item->P_APP_OBJECT_TYPE_ID).' '.form_hidden('app_rmdis_object_id[]', $item->P_APP_RMDIS_OBJECT_ID).' '.form_hidden('app_menu_profile_id[]', $app_menu_profile_id).'</td>';
+            echo '</tr>';        
+        }
+        
+        echo '</table>';
+        echo '<input type="hidden" name="'.$this->security->get_csrf_token_name().'" value="'.$this->security->get_csrf_hash().'">';
+        echo '</form>';
+        
+        
+    }
+    
+    public function setPrivilegeMenu() {
+            
+        $object_type_id = $this->input->post('object_type_id');
+        $opt_status = $this->input->post('opt_status');
+        $p_app_rmdis_object_id = $this->input->post('app_rmdis_object_id');
+        $p_app_menu_profile_id = $this->input->post('app_menu_profile_id');
+        
+        $return_id = "";
+        foreach($object_type_id as $idx => $p_app_object_type_id) {
+            if($p_app_rmdis_object_id[$idx] == "") {
+                //eksekusi insert    
+                $ins_array = array( 'P_APP_RMDIS_OBJECT_ID' => $this->Gen_id->generate_id('P_APP_RMDIS_OBJECT_ID','P_APP_RMDIS_OBJECT')->N,
+                                    'APP_MENU_PROFILE_ID' => $p_app_menu_profile_id[$idx],
+                                    'P_APP_OBJECT_TYPE_ID' => $p_app_object_type_id,
+                                    'IS_ACTIVE' => $opt_status[$idx]);
+                $this->M_admin->insRmdisObject($ins_array);
+            }else {
+                //eksekusi update
+                $upd_array = array('P_APP_RMDIS_OBJECT_ID' => $p_app_rmdis_object_id[$idx],
+                                    'IS_ACTIVE' => $opt_status[$idx]);
+                $this->M_admin->updRmdisObject($upd_array); 
+            }
+            
+            $return_id = $p_app_menu_profile_id[$idx];
+        }
+        echo $return_id;
     }
 }
