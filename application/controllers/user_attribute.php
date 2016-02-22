08bd9df4d42d15801115c3fa195ebcde7982837c
wiliamdecosta <wiliamdecosta@gmail.com>
2016-02-22 10:31:10 +0700

    ''

--- application/controllers/user_attribute.php
+++ application/controllers/user_attribute.php
@@ -6,7 +6,7 @@ class User_attribute extends CI_Controller {
 
 	function __construct() {
 		parent::__construct();
-
+        date_default_timezone_set('Asia/Jakarta');
         checkAuth();
         $this->load->model('M_jqGrid', 'jqGrid');
 	}
