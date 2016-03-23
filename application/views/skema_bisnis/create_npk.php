f4110eb6ff8c4b00bf64c56224c01eec958c062f
wiliamdecosta <wiliamdecosta@gmail.com>
2016-03-23 20:47:35 +0700

    ''

--- application/views/skema_bisnis/create_npk.php
+++ application/views/skema_bisnis/create_npk.php
@@ -4,89 +4,191 @@
             <form class="form-horizontal" role="form" id="form_calculate_mf">
                 <div class="form-group">
                     <div class="row">
-                        <div class="row">
-                            <label class="col-xs-1"> Periode </label>
-                            <div class="col-xs-2">
-                                <?php echo bulan('', date('m')); ?>
-                            </div>
+                        <label class="col-sm-2 col-xs-12 control-label no-padding-right"> Periode </label>
+                        <div class="col-sm-2 col-xs-6">
+                            <?php echo bulan('', date('m')); ?>
+                        </div>
+                        <div class="col-sm-2 col-xs-6">
+                            <?php echo tahun('', date('Y')); ?>
                         </div>
                     </div>
-
-                    <div class="col-xs-2">
-                        <?php echo tahun('', date('Y')); ?>
-                    </div>
                 </div>
+
                 <div class="form-group">
-                    <label class="col-sm-1 control-label no-padding-right"></label>
-                    <div class="col-sm-11" id="gridCalculate">
-                        <!--                        --><?php //$this->load->view('skema_bisnis/grid_skembis_calculate'); ?>
-                    </div>
+                    <div class="row">
+                        <div class="col-sm-2 col-xs-4">
+                            <label class="col-xs-12 control-label no-padding-right"> Pilih Skema </label>
+                        </div>
+                        <div class="col-sm-4 col-xs-4">
+                            <select name="opt_npk_fee_id" class="col-xs-12" id="opt_npk_fee_id">
+                            </select>
+                        </div>
 
+                        <div class="col-sm-2 col-xs-4">
+                            <a class="btn btn-sm btn-primary" id="btn_view_skema">
+                            <i class="ace-icon fa fa-eye align-top bigger-125"></i>
+                            Lihat NPK
+                            </a>
+                        </div>
+
+                    </div>
                 </div>
-                <a class="btn btn-sm btn-success" id="view_calculate">
-                    <i class="ace-icon fa fa-eye align-top bigger-125"></i>
-                    Lihat Skema
-                </a>
+
             </form>
+        </div>
+    </div>
+    
+    <div class="row" style="margin:5px;">
+        <div class="col-xs-12" id="report_npk" style="display:none;padding:5px;border:1px solid #C0C0C0;">
+            <div>
+                <a class="btn btn-sm btn-success" id="btn_export_excel_npk">
+                <i class="ace-icon ace-icon fa fa-print align-top bigger-125"></i>
+                Export to Excel
+                </a>
+                
+                <a class="btn btn-sm btn-danger" id="btn_lock_skema">
+                <i class="ace-icon ace-icon fa fa-lock align-top bigger-125"></i>
+                    Lock Skema
+                </a>
+            </div>
+            <div class="col-xs-12" id="content_npk"></div>
         </div>
     </div>
 </div>
 <script type="text/javascript">
-    //  $('#gridCalculate').show();
 
-    $('#btn_calculate').click(function () {
-        var skema_id = $('#form_skema').val();
-        var bulan = $('#bulan').val();
-        var tahun = $('#tahun').val();
-        var pgl_id = '<?php echo $pgl_id;?>';
-        if (skema_id) {
-            $.ajax({
+
+    $(function() { /* on ready*/
+
+        fillSelectOptionSkema(getPeriod());
+        
+        $('#bulan').on('change', function (e) {
+            fillSelectOptionSkema(getPeriod());
+            clearNPKReport();
+        });
+
+        $('#tahun').on('change', function (e) {
+            fillSelectOptionSkema(getPeriod());
+            clearNPKReport();
+        });
+        
+        $('#btn_view_skema').on('click', function (e) {
+            if( $('#opt_npk_fee_id').val() == "" ) return;
+            clearNPKReport();
+            fillNPKReport();
+        });
+        
+        $('#btn_export_excel_npk').on('click', function (e) {
+            exportExcelNPKReport();        
+        });
+        
+        $('#btn_lock_skema').on('click', function (e) {
+            lockSkema();     
+        });
+    });
+    
+    function clearNPKReport() {
+        $('#content_npk').html(""); 
+        $('#report_npk').hide();
+    }
+    
+    function getPeriod() {
+        var bulan = $("#bulan").val();
+        var tahun = $("#tahun").val();
+
+        var period = tahun + "" + bulan;
+        return period;    
+    }
+    
+    
+    function cekLockedSkema() {
+         $.ajax({
+            url: "<?php echo base_url();?>skema_bisnis/isSkemaLock",
+            type: "POST",
+            data: { npk_fee_id: $('#opt_npk_fee_id').val() },
+            success: function (data) {
+                if(data == "")
+                    $('#btn_lock_skema').show();
+                else
+                    $('#btn_lock_skema').hide();
+                
+                $('#report_npk').show();
+            }
+        });
+    }
+    
+    function fillNPKReport() {
+        $.ajax({
+            url: "<?php echo base_url();?>skema_bisnis/htmlNPKReport",
+            type: "POST",
+            data: { npk_fee_id: $('#opt_npk_fee_id').val(),
+                    period : getPeriod(),
+                    bulan : $("#bulan option:selected").text(),
+                    tahun : $("#tahun option:selected").text(),
+                    schm_fee_name : $("#opt_npk_fee_id option:selected").text()
+                  },
+            success: function (data) {
+                $('#content_npk').html("");
+                $('#content_npk').html(data);
+                cekLockedSkema();
+            }
+        });
+    }
+    
+    function exportExcelNPKReport() {
+        var url = "<?php echo base_url();?>skema_bisnis/excelNPKReport?";
+        url += "npk_fee_id=" + $('#opt_npk_fee_id').val();
+        url += "&period=" + getPeriod();
+        url += "&bulan=" + $("#bulan option:selected").text();
+        url += "&tahun=" + $("#tahun option:selected").text();
+        url += "&schm_fee_name=" + $("#opt_npk_fee_id option:selected").text();
+        url += "&<?php echo $this->security->get_csrf_token_name(); ?>=<?php echo $this->security->get_csrf_hash(); ?>";
+        window.location = url;
+    }
+    
+    function fillSelectOptionSkema(period) {
+        $.ajax({
                 // async: false,
-                url: "<?php echo base_url();?>skema_bisnis/proses_calculate",
-                type: "POST",
-                dataType: "json",
-                data: {skema_id: skema_id, bulan: bulan, tahun: tahun, pgl_id: pgl_id},
-                success: function (data) {
-                    if (data.success == true) {
-                        swal('', data.message, 'success');
-                        $("#gridCalculate").load("<?php echo base_url('skema_bisnis/grid_skembis_calculate'); ?>", {
-                            skema_id: skema_id,
-                            pgl_id: pgl_id
-                        }, function () {
-                        });
-
-                    } else {
-                        swal('', 'Error ', 'error')
+            url: "<?php echo base_url();?>skema_bisnis/getSkemaSelectOption",
+            type: "POST",
+            data: {period: period, pgl_id: '<?php echo $pgl_id; ?>'},
+            success: function (data) {
+                $('#opt_npk_fee_id').html("");
+                $('#opt_npk_fee_id').html(data);
+            }
+        });
+    }
+    
+    function lockSkema() {
+        swal({
+            title: "Konfirmasi Lock Skema",
+            text: "Apakah Anda yakin untuk mengunci skema berikut?",
+            type: "warning",
+            showCancelButton: true,
+            showLoaderOnConfirm: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Ya, Lock Skema",
+            cancelButtonText: "Tidak, Batalkan",
+            closeOnConfirm: false,
+            closeOnCancel: true
+        },
+        function (isConfirm) {
+            if (isConfirm) {
+                $.post("<?php echo base_url();?>skema_bisnis/lockSkemaNPK",
+                    { npk_fee_id: $('#opt_npk_fee_id').val() },
+                    function (response) {
+                        var response = JSON.parse(response);
+                        if (response.success == false) {
+                            swal("Perhatian", response.message, "warning");
+                        } else {
+                            swal("Berhasil", response.message, "success");
+                            fillNPKReport();
+                        }
                     }
+                );
+            }
+        });    
 
-                }
-            });
-        } else {
-            swal('', 'Skema belum dipilih !', 'warning');
-            return false;
-        }
-    });
-
-    $('#view_calculate').click(function () {
-        var skema_id = $('#form_skema').val();
-        var pgl_id = '<?php echo $pgl_id;?>';
-        var bulan = $('#bulan').val();
-        var tahun = $('#tahun').val();
-        if (skema_id) {
-            $("#gridCalculate").load("<?php echo base_url('skema_bisnis/grid_skembis_calculate'); ?>", {
-                    skema_id: skema_id,
-                    pgl_id: pgl_id,
-                    bulan: bulan,
-                    tahun: tahun
-                },
-                function () {
-                });
-        } else {
-            swal('', 'Skema belum dipilih !', 'warning');
-            return false;
-        }
-
-    });
-
+    }
 
 </script>
